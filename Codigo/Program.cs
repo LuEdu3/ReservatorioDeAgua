a6eb53cccecf032490ffd810757c165fdf016db0
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2024-10-07 13:57:01 -0300

    adicionado correcoes

--- Codigo/Program.cs
+++ Codigo/Program.cs
@@ -1,21 +1,44 @@
-﻿bool boiaA =true;
-bool boiaB =true;
-bool boiaC =true;
+﻿bool boiaA = true;
+bool boiaB = true;
+bool boiaC = true;
+bool eletroValvS1 = true;
+bool eletroValvS2 = true;
+bool erro = true;
 
 Console.WriteLine("Estados das boias:");
-if(boiaA == false && boiaB ==false && boiaC ==false)
+if (boiaA == false && boiaB == false && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == false)
 {
-    Console.WriteLine("Reservatorios todos vazios, ligar eletroválvula S2 ");
+    Console.WriteLine("0");
 }
-if(boiaA ==true && boiaB == false && boiaC == false)
+if (boiaA == false && boiaB == false && boiaC == true && eletroValvS1 == false && eletroValvS2 == true && erro == false)
 {
-    Console.WriteLine("Bomba ligada para a caixa de água");
+    Console.WriteLine("1");
 }
-if(boiaA ==true && boiaB == false && boiaC ==true)
+if (boiaA == false && boiaB == true && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == true)
 {
-    Console.WriteLine("Caixa Cheia, enchendo reservatório até boia B");
+    Console.WriteLine("2");
 }
-// if(boiaA ==true && boiaB ==true && boiaC ==true)
-// {
-//     Console.WriteLine("Todas os reservatórios cheios. Parar eletroválvula S2");
-// }
+if (boiaA == true && boiaB == false && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == true)
+{
+    Console.WriteLine("3");
+}
+if (boiaA == true && boiaB == false && boiaC == true && eletroValvS1 == true && eletroValvS2 == true && erro == false)
+{
+    Console.WriteLine("4");
+}
+if (boiaA == true && boiaB == true && boiaC == false && eletroValvS1 == true && eletroValvS2 == true && erro == false)
+{
+    Console.WriteLine("5");
+}
+if (boiaA == true && boiaB == true && boiaC == false && eletroValvS1 == true && eletroValvS2 == false && erro == false)
+{
+    Console.WriteLine("6");
+}
+if (boiaA == true && boiaB == true && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == false)
+{
+    Console.WriteLine("7");
+}
+else
+{
+    Console.WriteLine("8");
+}

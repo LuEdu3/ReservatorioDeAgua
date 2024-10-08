8ab82109311356cd9778b140ab6df28406fdef09
Luiz Eduardo <Luizinformaticaeduardo@gmail.com>
2024-10-08 08:08:30 -0300

    Adicionado codigo

--- Codigo/Program.cs
+++ Codigo/Program.cs
@@ -1,44 +1,90 @@
-﻿bool boiaA = true;
-bool boiaB = true;
-bool boiaC = true;
-bool eletroValvS1 = true;
-bool eletroValvS2 = true;
-bool erro = true;
+﻿Console.Clear();
 
-Console.WriteLine("Estados das boias:");
-if (boiaA == false && boiaB == false && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == false)
+bool boiaaA =true;
+int boiaA;
+bool boiaaB = true;
+int boiaB;
+bool boiaaC = true;
+int boiaC;
+// bool eletroValvS1 = true;
+// bool eletroValvS2 = true;
+// bool erro = true;
+
+Console.WriteLine("Insira o Estado das boia A:");
+if(int.TryParse(Console.ReadLine(),out boiaA))
 {
-    Console.WriteLine("0");
+    if(boiaA == 0)
+    {
+        boiaaA =false;
+        Console.WriteLine("Bomba desligada");
+    }
+    else if(boiaA ==1)
+    {
+        Console.WriteLine("Nível A do Reservátorio alcançado, ligando bomba.");
+    }
 }
-if (boiaA == false && boiaB == false && boiaC == true && eletroValvS1 == false && eletroValvS2 == true && erro == false)
+Console.WriteLine("Insira o Estado das boia B:");
+if(int.TryParse(Console.ReadLine(),out boiaB))
 {
-    Console.WriteLine("1");
+    if(boiaB == 0)
+    {
+        boiaaB =false;
+        Console.WriteLine("adssdas");
+    }
+    else if(boiaB ==1 && boiaA ==1)
+    {
+        Console.WriteLine("Valvula S2 desligada");
+    }
 }
-if (boiaA == false && boiaB == true && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == true)
+Console.WriteLine("Insira o Estado das boia C:");
+if(int.TryParse(Console.ReadLine(),out boiaC))
 {
-    Console.WriteLine("2");
+    if(boiaC == 0)
+    {
+        boiaaA =false;
+        Console.WriteLine("Caixa d'agua 2 precisando de agua");
+    }
+    else if(boiaC ==1)
+    {
+        Console.WriteLine("Bomba C Ligada");
+    }
 }
-if (boiaA == true && boiaB == false && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == true)
-{
-    Console.WriteLine("3");
-}
-if (boiaA == true && boiaB == false && boiaC == true && eletroValvS1 == true && eletroValvS2 == true && erro == false)
-{
-    Console.WriteLine("4");
-}
-if (boiaA == true && boiaB == true && boiaC == false && eletroValvS1 == true && eletroValvS2 == true && erro == false)
-{
-    Console.WriteLine("5");
-}
-if (boiaA == true && boiaB == true && boiaC == false && eletroValvS1 == true && eletroValvS2 == false && erro == false)
-{
-    Console.WriteLine("6");
-}
-if (boiaA == true && boiaB == true && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == false)
-{
-    Console.WriteLine("7");
-}
-else
-{
-    Console.WriteLine("8");
-}
+
+
+// Console.WriteLine("Estados das boias:");
+// if (boiaA == false && boiaB == false && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == false)
+// {
+//     Console.WriteLine("0");
+// }
+// if (boiaA == false && boiaB == false && boiaC == true && eletroValvS1 == false && eletroValvS2 == true && erro == false)
+// {
+//     Console.WriteLine("1");
+// }
+// if (boiaA == false && boiaB == true && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == true)
+// {
+//     Console.WriteLine("2");
+// }
+// if (boiaA == true && boiaB == false && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == true)
+// {
+//     Console.WriteLine("3");
+// }
+// if (boiaA == true && boiaB == false && boiaC == true && eletroValvS1 == true && eletroValvS2 == true && erro == false)
+// {
+//     Console.WriteLine("4");
+// }
+// if (boiaA == true && boiaB == true && boiaC == false && eletroValvS1 == true && eletroValvS2 == true && erro == false)
+// {
+//     Console.WriteLine("5");
+// }
+// if (boiaA == true && boiaB == true && boiaC == false && eletroValvS1 == true && eletroValvS2 == false && erro == false)
+// {
+//     Console.WriteLine("6");
+// }
+// if (boiaA == true && boiaB == true && boiaC == false && eletroValvS1 == false && eletroValvS2 == true && erro == false)
+// {
+//     Console.WriteLine("7");
+// }
+// else
+// {
+//     Console.WriteLine("8");
+// }
